d466100cced76e83b214da676517365b0c1baa39
Carlo Nucera <meditans@gmail.com>
2023-12-03 19:25:01 +0100

    First release

---
+++ pack.pl
@@ -0,0 +1,10 @@
+name(medikit).
+title('meditans\' personal utilities').
+
+version('0.0.1').
+download('https://github.com/meditans/medikit/archive/v0.0.1.zip').
+
+author('Carlo Nucera','meditans@gmail.com').
+packager('Carlo Nucera','meditans@gmail.com').
+maintainer('Carlo Nucera','meditans@gmail.com').
+home('https://github.com/meditans/medikit').
---
+++ prolog/medikit.pl
@@ -0,0 +1,50 @@
+:- module(medikit, [
+                      % More operations on lists
+                      list_sum/2,
+                      list_product/2,
+
+                      % Operation on subterms
+                      subterms/3,
+
+                      % Utilities for the chr store
+                      find_constraint/2,
+                      local_chr/3,
+
+                      % Function expansion clauses for ξ
+                      function_expansion/3
+                  ]).
+
+:- use_module(library(chr)).
+:- use_module(library(function_expansion)).
+
+
+% More list-related utilities
+
+list_product(L, Prod) :-
+    foldl([A,B,C] >> (C is A*B), L, 1, Prod).
+
+list_sum(L, Sum) :- sum_list(L, Sum).
+
+
+% Subterms matching a given predicate
+
+subterms(Whole, Goal, Subterms) :-
+    foldsubterms({Goal}/[A,S0,S1] >> (call(Goal,A), S1=[A|S0]),
+                 Whole, [], Subterms).
+
+
+% CHR utilities
+
+find_constraint(Goal, Cs) :-
+    findall(Goal, find_chr_constraint(Goal), Cs).
+
+local_chr(Facts, Result, Res) :-
+    thread_create((maplist(call, Facts),
+                   find_constraint(Result, Ns),
+                   thread_exit(Ns)), Id),
+    thread_join(Id, exited(Res)).
+
+
+% Macros for writing arithmetic
+
+user:function_expansion(ξ(X), Y, Y #= X).
